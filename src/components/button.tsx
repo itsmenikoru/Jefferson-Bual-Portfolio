0d4623750950d079a7185d7aeb389db3046c4556
Jefferson Bual <bualjefferson64@gmail.com>
2024-09-22 11:25:05 +0800

    updated cv

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,9 +4,8 @@ export default function Button() {
     <button
       onClick={() =>
         window.open(
-          'https://drive.google.com/uc?export=download&id=1bsjm_NJApPTj_SSvX7c_uXyGABsDTHRs'
+          'https://drive.google.com/uc?export=download&id=1Kc-JUMr8qPvlkUt-ySApAqAe0YudgjgL'
         )
-
 
       }
       className='bg-secondary w-fit p-3 text-[11px] rounded-md text-white hover:bg-button_hover transition-all transform hover:px-6 hover:scale-100 duration-300'>
