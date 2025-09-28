201dcaa73e0422d58d56522e259c4737438334cb
itsmenikoru <bualjefferson64@gmail.com>
2025-09-28 21:28:17 +0800

    Modify experience page

--- src/app/(pages)/skills&experiences/page.tsx
+++ src/app/(pages)/skills&experiences/page.tsx
@@ -11,12 +11,57 @@ export default function SkillsExpPage() {
 
   const experience = [
     {
-      title: 'OIC - Reshipper',
+      title: 'Junior Data Analyst',
+      year: 'Nov 2024 -  Present',
+      description: 'Developed automated reporting solutions and performed efficient ETL (Extract, Transform, Load) processes. Focused on transforming raw data into actionable insights through custom report development, backend scripting, and data analysis.'
+    },
+    {
+      title: 'Backend Developer Intern at Pixel8 Web Solutions & Consultancy Inc.',
+      year: 'Jan 2024 -  2021',
+       description: 'Developed and tested APIs to handle success and error responses, and collaborated with the team to ensure seamless integration.'
+    },
+    {
+      title: 'OIC - Reshipper at Lazada',
       year: 'Mar 2020 - Jan 2021',
       description: 'Managed a team and tracked performance using Excel to maintain transactions and monitor KPIs.',
     },
   ];
   const frameworks = [
+     {
+      src: '/python.png',
+      alt: 'react',
+      title: 'Python',
+      description:
+        '',
+    },
+    {
+      src: '/airflow(4).png',
+      alt: 'react',
+      title: 'Apache Airflow',
+      description:
+        '',
+    },
+    {
+      src: '/java-script.png',
+      alt: 'react',
+      title: 'JS',
+      description:
+        '',
+    },
+    {
+      src: '/php.png',
+      alt: 'react',
+      title: 'Php',
+      description:
+        '',
+    },
+    {
+      src: '/sql.png',
+      alt: 'react',
+      title: 'MySQL',
+      description:
+        '',
+    },
     {
       src: '/excel.png',
       alt: 'react',
@@ -31,35 +76,21 @@ export default function SkillsExpPage() {
       description:
         '',
     },
-    {
-      src: '/sql.png',
-      alt: 'react',
-      title: 'MySQL',
-      description:
-        '',
-    },
-    {
-      src: '/postgre.png',
-      alt: 'react',
-      title: 'PostgreSQL',
-      description:
-        '',
-    },
-    {
-      src: '/python.png',
-      alt: 'react',
-      title: 'Python',
-      description:
-        '',
-    },
     
-    {
-      src: '/gpt.png',
-      alt: 'react',
-      title: 'Chat GPT',
-      description:
-        '',
-    },
+    // {
+    //   src: '/postgre.png',
+    //   alt: 'react',
+    //   title: 'PostgreSQL',
+    //   description:
+    //     '',
+    // },
+    // {
+    //   src: '/gpt.png',
+    //   alt: 'react',
+    //   title: 'Chat GPT',
+    //   description:
+    //     '',
+    // },
     {
       src: '/github.png',
       alt: 'react',
@@ -67,13 +98,13 @@ export default function SkillsExpPage() {
       description:
         '',
     },
-    {
-      src: '/vs.png',
-      alt: 'react',
-      title: 'Visual Studio Code',
-      description:
-        '',
-    },
+    // {
+    //   src: '/vs.png',
+    //   alt: 'react',
+    //   title: 'Visual Studio Code',
+    //   description:
+    //     '',
+    // },
   ];
 
   return (
@@ -99,35 +130,29 @@ export default function SkillsExpPage() {
                 ))}
               </div>
             </div>
-            <div className=''>
-              <DetailsTitle title='Tools' bg_color='white' />
-              <div className='grid grid-cols-4 gap-3 h-full mt-3'>
-                {frameworks.map((data, index) => (
+           <div className='grid grid-cols-4 gap-3 h-full mt-3'>
+              {frameworks.map((data, index) => (
+                <div
+                  key={index}
+                  className='bg-[#575757] rounded-md flex flex-col justify-center items-center p-3'
+                >
                   <div
-                    key={index}
-                    className=' bg-[#575757] rounded-md flex flex-col justify-center items-center p-3 transition-all transform hover:px-6 hover:scale-110 duration-300'>
-                    <div className='w-[25px] '>
-                      <Image
-                        src={data.src}
-                        width={50}
-                        height={50}
-                        alt='linkedin icon'
-                      />
-                    </div>
+                    className={`flex items-center justify-center ${
+                      data.title === 'Apache Airflow' ? 'w-[80px] h-[80px] group' : 'w-[25px] h-[80px] group'
+                    }`}
+                  >
+                    <Image
+                      src={data.src}
+                      width={data.title === 'Apache Airflow' ? 80 : 60}
+                      height={data.title === 'Apache Airflow' ? 80 : 60}
+                      alt={data.alt}
+                      className="object-contain transition-transform duration-300 group-hover:scale-125"
+                    />
                   </div>
-                ))}
-              </div>
-              {/* <div className='flex justify-between p-3 text-[11px]'>
-                <div></div>
-                <button
-                  disabled={isDisabled}
-                  className={`underline ${
-                    isDisabled ? 'text-gray-600' : 'text-white'
-                  }`}>
-                  See more...
-                </button>
-              </div> */}
+                </div>
+              ))}
             </div>
+
           </div>
         </div>
       </div>
